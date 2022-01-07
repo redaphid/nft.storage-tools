62075cb53ce5b95213d11fd99ff43c3e6b0725fd
redaphid <iam@aaronherres.com>
2022-01-06 19:09:06 -0700

    trying the car streaming api

--- test/directory-upload.test.js
+++ test/directory-upload.test.js
@@ -1,10 +1,18 @@
-import { directoryUpload } from "../lib/directory-upload.js"
+import {mkdirSync} from "fs"
+import { directoryUpload } from "../lib/directory-upload"
 describe("Directory Uploading", () => {
   xdescribe("directoryUpload", () => {
     it("should exist", () => {
       expect(directoryUpload).toBeDefined()
     })
     describe("given a directory with no files", () => {
+      beforeEach(() => {
+        try {
+          //Unfortunately, you can't commit an empty directory to git.
+          mkdirSync("./test/data/empty-directory")
+        }
+        catch(e){}
+      })
       it("should return an empty array", async () => {
         const results = await directoryUpload("./test/data/empty-directory")
         expect(results).toEqual([])
