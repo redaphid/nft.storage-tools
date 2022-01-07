394133881d8fcb2d9b526eba5647b81a4a645331
redaphid <iam@aaronherres.com>
2022-01-07 13:34:49 -0700

    restore old walking tests

---
+++ test/get-file-cids-from-folder.test.js
@@ -0,0 +1,68 @@
+import { getIpfsDirectoryInfo } from "../lib/get-file-cids-from-folder"
+import { create as ipfsHttpCreate } from "ipfs-http-client"
+import { map, path } from "ramda"
+
+// Note: to avoid rate limiting, consider running a local ipfs daemon and running the tests like this:
+// IPFS_URL="http://localhost:5001/api/v0" npm run test 
+// This also caches the files locally, greatly speeding up the tests.
+
+const IPFS_URL = process.env.IPFS_URL || "https://dweb.link/api/v0"
+
+const getFileCount = (dir) => {
+  return Object.keys(dir).length
+}
+
+const getFiles = map(
+  path(["files"])
+)
+const ls  = (f)=>{
+  return map(ls, getFiles(f))  
+}
+
+describe("Directory Walking in IPFS", () => {
+  describe("directoryWalk", () => {
+    it("should exist", () => {
+      expect(getIpfsDirectoryInfo).toBeDefined()
+    })
+  })
+  describe("given an ipfs client", () => {
+    let ipfs
+    beforeAll(async () => {
+      ipfs = ipfsHttpCreate(IPFS_URL)
+      await ipfs.id
+    })
+    describe("given a known directory in ipfs (containing the node_modules of this project, as a matter of fact)", () => {
+      let dirinfo
+
+      beforeAll(async () => {
+        const node_modules_cid = "bafybeidl7ozkgaya4jb6tt3ey5t7pw7uefjfdirwfnnplrim2ksw7a4doi"
+        dirinfo = await getIpfsDirectoryInfo(node_modules_cid, ipfs)
+        // console.log(JSON.stringify(dirinfo, null, 2))
+        console.log(JSON.stringify(ls(dirinfo),null, 2))
+      })
+
+      it("should return an array with 1 folder", () => {
+        const length = getFileCount(dirinfo)
+        expect(length).toEqual(1)
+      })
+
+      it("should be a directory called 'data'", () => {
+        const dir = dirinfo["data"]
+        expect(dir.name).toEqual("data")
+        expect(dir.type).toEqual("dir")        
+      })
+
+      it("should have 4 files in it", () => {
+        const dir = dirinfo["data"]
+        const length = getFileCount(dir.files)
+
+        expect(length).toEqual(4)
+      })
+      it("should have cids", () => {
+        const cid = dirinfo["data"].cid
+        expect(cid).toBeDefined()
+        console.log(cid)
+      })
+    })
+  })
+})
