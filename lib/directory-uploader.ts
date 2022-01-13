6a8ad92283fb560199e34de5fec583d2f1e5bbf4
redaphid <iam@aaronherres.com>
2022-01-13 00:02:03 -0700

    base case (finally) for the tests

---
+++ lib/directory-uploader.ts
@@ -0,0 +1,30 @@
+class DirectoryUploader {
+    constructor(directory, bucket, options) {
+        // this.directory = directory;
+        // this.bucket = bucket;
+        // this.options = options;
+    }
+    upload() {
+    //     return __awaiter(this, void 0, void 0, function* () {
+    //         const { directory, bucket, options } = this;
+    //         const files = yield readdir(directory);
+    //         const uploadPromises = files.map((file) => __awaiter(this, void 0, void 0, function* () {
+    //             const filePath = path.join(directory, file);
+    //             const fileName = path.basename(filePath);
+    //             const fileStream = fs.createReadStream(filePath);
+    //             const upload = new s3.ManagedUpload({
+    //                 ...options,
+    //                 params: {
+    //                     Bucket: bucket,
+    //                     Key: fileName,
+    //                     Body: fileStream,
+    //                 },
+    //             });
+    //             return upload.promise();
+    //         }));
+    //         return Promise.all(uploadPromises);
+    //     });
+    // }
+}
+}
+export {DirectoryUploader}
--- lib/upload-directory.test.ts
+++
@@ -1,6 +0,0 @@
-import { uploadDirectory } from "./upload-directory"
-describe("Upload Directory", () => {
-  it("should exist", ()=>{
-    expect(uploadDirectory).toBeDefined()
-  })
-})
