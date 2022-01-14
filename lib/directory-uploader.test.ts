c99495afc76c50515cb2c521dcf8108563970591
redaphid <iam@aaronherres.com>
2022-01-13 21:19:12 -0700

    moving to the more annoying conditional mocking :(. It's too bad we can't use the 'real' nftstorage client. It's a weird Jest limitation I ran into

--- lib/directory-uploader.test.ts
+++ lib/directory-uploader.test.ts
@@ -25,7 +25,11 @@ describe("Upload Directory", () => {
         });
         describe("when the store method resolves", () => {
           beforeEach(async () => {
-            client.store.mockResolvedValue({ ipnft: "frankenstein-nft", url: "frankenstein-url" });
+            // client.store.mockResolvedValue({ ipnft: "frankenstein-nft", url: "frankenstein-url" });
+            client.store.mockImplementationOnce(() => {
+              return Promise.resolve({ ipnft: "frankenstein-nft", url: "frankenstein-url" })
+            })
+
             await uploaderPromise;
           });
           it("should tell us the frankenstein file is complete, w/the nft info", () => {
